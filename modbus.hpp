e844413f3a846f698cf317a38cfca9276615c5db
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-02-19 10:08:31 +0800

    Clear

---
+++ modbus.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "modbus/mbdevice.hpp"
+#include "modbus/mbclient.hpp"
+#include "modbus/exception.hpp"
+
+#include "shared/databytes.hpp"
---
+++ mrit.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "mrit/mrmaster.hpp"
+
+#include "shared/databytes.hpp"
+#include "modbus/exception.hpp"
