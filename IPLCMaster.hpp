5605b98e9b42eee43a7456ebdb749aa0953a33d0
WarGrey Ju <WarGrey Ju>
2019-03-11 12:19:13 +0800

    abstract plt connection feedback mechanism

--- IPLCMaster.hpp
+++ IPLCMaster.hpp
@@ -3,32 +3,13 @@
 #include "network/tcp.hpp"
 
 namespace WarGrey::SCADA {
-	class IPLCMaster;
-
-	private class IPLCStatusListener {
+	private class IPLCStatusListener : public WarGrey::SCADA::ITCPStatusListener {
 	public:
-		virtual void on_plc_connectivity_changed(WarGrey::SCADA::IPLCMaster* master, bool connected) {}
-
-	public:
-		virtual void on_send_data(WarGrey::SCADA::IPLCMaster* master, long long bytes, double span_ms, double timestamp_ms) {}
-		virtual void on_receive_data(WarGrey::SCADA::IPLCMaster* master, long long bytes, double span_ms, double timestamp_ms) {}
-		virtual void on_confirm_data(WarGrey::SCADA::IPLCMaster* master, long long bytes, double span_ms, double timestamp_ms) {}
+		virtual void on_confirm_data(WarGrey::SCADA::ITCPConnection* master, long long bytes, double span_ms, double timestamp_ms) {}
 	};
 
-	private class IPLCMaster abstract : public WarGrey::SCADA::ITCPConnection {
+	private class IPLCMaster abstract : public WarGrey::SCADA::ITCPFeedBackConnection<WarGrey::SCADA::IPLCStatusListener> {	
 	public:
-		virtual ~IPLCMaster() noexcept;
-
-	public:
-		void set_suicide_timeout(long long ms);
-		void push_plc_status_listener(WarGrey::SCADA::IPLCStatusListener* listener);
-		void notify_connectivity_changed();
-		void notify_data_sent(long long bytes, double span_ms);
-		void notify_data_received(long long bytes, double span_ms);
 		void notify_data_confirmed(long long bytes, double span_ms);
-
-	private:
-		std::list<WarGrey::SCADA::IPLCStatusListener*> listeners;
-		Platform::Object^ killer;
 	};
 }
